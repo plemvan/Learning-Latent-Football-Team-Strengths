11f7a4644cb88204f205f91fc7fb969cda251456
Leo DONY <leo.dony@orange.fr>
2026-01-05 19:32:48 +0100

    Data Processing 1

---
+++ Data/data_cleaning.py
@@ -0,0 +1,111 @@
+#========== Data Cleaning and pre-processing ==========#
+
+## Imports
+import numpy as np
+import pandas as pd
+from typing import Union
+
+#======================================================#
+
+
+class Datacleaner:
+    """Class for the initial cleaning of the database"""
+
+    def __init__(self, filepath: str):
+
+        """
+        Data Cleaner
+
+        Parameters
+        ----------
+        filepath : str
+            Path to the data file
+        """
+
+        self.df = pd.read_csv(filepath)
+
+        self.interest_columns = ["Div", "Date", "Time", "HomeTeam", "AwayTeam",
+                                 "FTHG", "HG", "FTAG", "AG", "FTR", "Res",
+                                 "HTHG", "HTAG", "HTR", "Attendance", "Referee",
+                                 "HS", "AS", "HST", "AST", "HHW", "AHW",
+                                 "HC", "AC", "HF", "AF", "HFKC", "AFKC",
+                                 "HO", "AO", "HY", "AY", "HR", "AR",
+                                 "HBP", "ABP"]
+    
+
+    def drop_useless_columns(self):
+
+        """
+        Drops useless columns from the dataset using the list of columns of interest
+        """
+
+        self.df = self.df[[col for col in self.interest_columns if col in self.df.columns]]
+
+        return self
+    
+    
+    def drop_rows(self, row_idxs : list[int]):
+
+        """
+        Drops rows containing only NaN
+
+        Parameters
+        ----------
+
+        row_idxs : List[int]
+            List of row indexes
+        """
+
+        self.df = self.df.drop(index=row_idxs)
+
+        return self
+
+    
+
+    def fillNA(self, column: str, value : Union[str, int, float]):
+
+        """
+        Replace NaN occurences from a column with a given value
+
+        Parameters
+        ----------
+
+        column: str
+            Name of the column in which the replacement is made
+        value : str | int | float
+            Value of replacement. Should match the type of the column
+        """
+
+        if column not in self.df.columns:
+            raise ValueError(f"Column '{column}' does not exist in the dataframe.")
+
+        try:
+            self.df[column] = self.df[column].fillna(value)
+        except (ValueError, TypeError) as e:
+            raise TypeError(f"Impossible to fill '{column}' with {value} : {e}")
+
+        return self
+
+
+
+    def save_data(self, output_path : str, returns : bool = False):
+
+        """
+        Save the data set to .csv format
+        
+        Parameters
+        ----------
+        output_path : str
+            Path for the ouptut .csv file
+        returns : bool
+            Choice to return or not the DataFrame
+        """
+
+        self.df.to_csv(output_path, index=False)
+
+        if returns:
+            return self.df
+        return
+
+
+    
