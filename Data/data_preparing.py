cfec91cb4b44dcd88c3cb1932979f918707e7954
Leo DONY <leo.dony@orange.fr>
2026-01-16 17:20:37 +0100

    Adapting Default Stats to handle Paris FC

--- Data/data_preparing.py
+++ Data/data_preparing.py
@@ -290,10 +290,13 @@ class DataPreparer:
         self.df_merged = self.df_merged.rename(columns=rename_dict)
 
         # Fill Stats NaNs (Promoted teams)
-        defaults = {'Strength_Rank_Based': 0.05, 'Pythagorean_Exp': 0.35, 'Avg_Goals_Scored': 0.8, 'Home_Dependency': 0.5}
-        for col in feature_names:
-            self.df_merged[col + '_Home'] = self.df_merged[col + '_Home'].fillna(defaults[col])
-            self.df_merged[col + '_Away'] = self.df_merged[col + '_Away'].fillna(defaults[col])
+        defaults = {'Strength_Rank_Based': 0.05, 'Pythagorean_Exp': 0.35, 'Avg_Goals_Scored': 0.8, 'Home_Dependency': 0.6,'Avg_Shots_Target': 3.0, 'Avg_Corners': 3.5, 'Avg_Cards': 2.2}
+        all_features = ['Strength_Rank_Based', 'Pythagorean_Exp', 'Avg_Goals_Scored', 'Home_Dependency','Avg_Shots_Target', 'Avg_Corners', 'Avg_Cards']
+        for col in all_features:
+            if f"{col}_Home" in self.df_merged.columns:
+                self.df_merged[f"{col}_Home"] = self.df_merged[f"{col}_Home"].fillna(defaults[col])
+            if f"{col}_Away" in self.df_merged.columns:
+                self.df_merged[f"{col}_Away"] = self.df_merged[f"{col}_Away"].fillna(defaults[col])
         
         # Target Variables
         self.df_merged['Target'] = self.df_merged['FTR'].map({'H': 1.0, 'A': 0.0, 'D': 0.5})
